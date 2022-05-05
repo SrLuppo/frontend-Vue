9a25e4167353d20d0a54e6350a0ac840686e7153
Adelano Luppo e Silva <92128439+SrLuppo@users.noreply.github.com>
2022-05-05 16:25:31 -0300

    Primeiras aulas, rotas e componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <loginComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import loginComponent from './components/loginComponent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    loginComponent,
   }
 }
 </script>
---
+++ src/components/loginComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Olá Vue</h1>
+        <p>Meu nome é {{ nome }} estou estudando para ser um {{ profissão}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'loginComponent',
+    data() {
+        return {
+            nome:"Adelano",
+            profissão: "Programador"
+        }
+    },
+}
+</script>
